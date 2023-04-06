6675bbe4faf5a16385ed916ccc7957f29c00efd2
thirumangaivasan <ktvasan@gmail.com>
2023-04-06 08:54:54 -0500

    updated report

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,18 @@ module.exports = defineConfig({
   e2e: {
     "watchForFileChanges": false,
     "defaultCommandTimeout": 4000,
-    setupNodeEvents(on, config) {
+    "reporter":"mochawesome",
+    "reporterOptions": {
+      "charts": true,
+      "overwrite": false,
+      "html": false,
+      "json": true,
+      "reportDir": "cypress/report/mochawesome-report"  
+     },
+     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+     },
   },
 });
+
+

12aed435502abe84225919a22fc5a35332448acb
thirumangaivasan <ktvasan@gmail.com>
2023-04-07 21:10:34 -0500

    updated reporter

--- cypress.config.js
+++ cypress.config.js
@@ -4,16 +4,16 @@ module.exports = defineConfig({
   e2e: {
     "watchForFileChanges": false,
     "defaultCommandTimeout": 4000,
-    "reporter":"mochawesome",
+    reporter: 'cypress-mochawesome-reporter',
     "reporterOptions": {
       "charts": true,
       "overwrite": false,
       "html": false,
       "json": true,
-      "reportDir": "cypress/report/mochawesome-report"  
      },
      setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
      },
   },
 });
